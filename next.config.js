ce7368c49db811b670ad4e2343fb51c281e54d48
anbo-minecon <adaniesbasilio@gmail.com>
2026-06-25 20:49:51 -0500

    modificacion en config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["res.cloudinary.com", "lh3.googleusercontent.com"],
+    domains: [
+      "res.cloudinary.com",
+      "lh3.googleusercontent.com",
+      "public.blob.vercel-storage.com",
+    ],
+  },
+  experimental: {
+    serverComponentsExternalPackages: ["@vercel/blob"],
   },
 };
 
